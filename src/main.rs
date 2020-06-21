cf7ff41c6c17ac8eb81f0f8d2d67cc091667b0d8
Schakkall <jeuriqueribeiro@gmail.com>
2020-06-20 21:07:58 -0300

    Fix: No draw bug on second window

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ pub fn main() {
     
     //std::thread::spawn(move || {
         show(800, 600);    
+        show(1024, 720);    
     //});
 
     println!("Hello, Out of the window! {}", utils::sum(0,1));
