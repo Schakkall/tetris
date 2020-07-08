4925f51026f895a281e84fdd53316c65aee10c1a
Schakkall <jeuriqueribeiro@gmail.com>
2020-07-08 20:07:07 -0300

    cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use utils;
+//use utils::functions::sum;
 use view::screen::show;
 
 use std::thread;
@@ -12,10 +12,10 @@ pub fn main() {
     show(720, 1024);//Wide screen    
 
     std::thread::spawn( || {    
-        let is_sum_an_error: Result<u32, &str> = Ok(utils::sum(0,1));
+        let is_sum_an_error: Result<u32, &str> = Ok(utils::functions::sum(0,1));
         assert_eq!(is_sum_an_error.is_err(), false);
     
-        println!("Hello, Out of the window! {}", utils::sum(0,1));        
+        println!("Hello, Out of the window! {}", utils::functions::sum(0,1));        
         thread::sleep(Duration::from_millis(6000));      
     });      
 }
--- src/utils/src/lib.rs
+++ src/utils/src/lib.rs
@@ -1,8 +1,1 @@
-/*
-    It can be done in other way. Like "C plus plus" and "C", it is possible publicate functions and "classes" headers and implement them in files with appropriate names.
-    Use it as a example of packaging or, in rusting words, crating.
-*/
-
-pub fn sum(a:u32, b:u32) -> u32 {
-    a + b
-}
+pub mod functions;
